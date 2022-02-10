e7c8f4c18c6f23823e760106018bed5a855dd849
Eung Jin Lee <oiojin831@gmail.com>
2022-02-11 02:10:00 +0900

    sorting ingredients

--- app/routes/ingredients/index.jsx
+++ app/routes/ingredients/index.jsx
@@ -20,45 +20,56 @@ export const loader = async () => {
 
 export default function Index() {
   const data = useLoaderData();
+  const [sort, setSort] = React.useState(true);
+  const sorted = data.sort((a, b) =>
+    a.name > b.name ? 1 : b.name > a.name ? -1 : 0
+  );
+  const sortedData = sort ? sorted : sorted.reverse();
+
   return (
-    <Flex flexFlow="wrap" justify="space-around">
-      {data.map((entry) => (
-        <Box
-          minW="400px"
-          as="section"
-          bg={useColorModeValue('gray.100', 'inherit')}
-          py="4"
-          px={{ md: '8' }}
-          key={entry.id}
-        >
-          <Card maxW="3xl" mx="auto">
-            <CardHeader
-              title={entry.name}
-              action={[
-                <Button
-                  as={Link}
-                  to={`/ingredients/${entry.id}`}
-                  variant="outline"
-                >
-                  수정
-                </Button>,
-                <DeleteAlert
-                  title="ingredient"
-                  url={`/ingredients/${entry.id}`}
-                />,
-              ]}
-            />
-            <CardContent>
-              <Property label="구매 가격" value={`${entry.price}원`} />
-              <Property label="총 용량" value={entry.volume} />
-              <Property
-                label="단위가격(1g, 1ml)"
-                value={`${(entry.price / entry.volume).toFixed(2)}원`}
+    <>
+      <Flex justify={'center'}>
+        <Button onClick={() => setSort((prev) => !prev)}>sort</Button>
+      </Flex>
+      <Flex flexFlow="wrap" justify="space-around">
+        {data.map((entry) => (
+          <Box
+            minW="400px"
+            as="section"
+            bg={useColorModeValue('gray.100', 'inherit')}
+            py="4"
+            px={{ md: '8' }}
+            key={entry.id}
+          >
+            <Card maxW="3xl" mx="auto">
+              <CardHeader
+                title={entry.name}
+                action={[
+                  <Button
+                    as={Link}
+                    to={`/ingredients/${entry.id}`}
+                    variant="outline"
+                  >
+                    수정
+                  </Button>,
+                  <DeleteAlert
+                    title="ingredient"
+                    url={`/ingredients/${entry.id}`}
+                  />,
+                ]}
               />
-            </CardContent>
-          </Card>
-        </Box>
-      ))}
-    </Flex>
+              <CardContent>
+                <Property label="구매 가격" value={`${entry.price}원`} />
+                <Property label="총 용량" value={entry.volume} />
+                <Property
+                  label="단위가격(1g, 1ml)"
+                  value={`${(entry.price / entry.volume).toFixed(2)}원`}
+                />
+              </CardContent>
+            </Card>
+          </Box>
+        ))}
+      </Flex>
+    </>
   );
 }
